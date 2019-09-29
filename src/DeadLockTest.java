e59a391c76494cf6c42a351624f3b04e86356e0c
ravindurashmin <rashmin.ravindu@gmail.com>
2019-09-29 21:08:54 +0530

    initial commit

--- src/DeadLockTest.java
+++ src/DeadLockTest.java
@@ -1,8 +1,12 @@
 public class DeadLockTest {
+
+    //There are two resources
     public final static String r1= "rrr1";
     public final static String r2 = "rrr2";
 
     public static void main(String[] args) {
+
+        //Thread 1 is trying to lock r1 first
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -14,6 +18,7 @@ public class DeadLockTest {
                         e.printStackTrace();
                     }
 
+                    //then tries to lock r2
                     synchronized (r2){
                         System.out.println("trying r2 ");
                     }
@@ -21,6 +26,7 @@ public class DeadLockTest {
             }
         });
 
+        //Thread 1 is trying to lock r2 first
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -32,6 +38,7 @@ public class DeadLockTest {
                         e.printStackTrace();
                     }
 
+                    //then r1
                     synchronized (r1){
                         System.out.println("trying r1 ");
                     }
@@ -39,6 +46,7 @@ public class DeadLockTest {
             }
         });
 
+        //this results in a deadlock where both threads are waiting for each one to finish the task
         t1.start();
         t2.start();
 
